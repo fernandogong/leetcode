77e1e6e82f7bb33c4112cb9dd553b44acc0c26de
Fernando Gong <115079460+gasoser@users.noreply.github.com>
2023-05-30 14:09:57 -0300

    commit

--- python/2. Add Two Numbers.py
+++ python/2. Add Two Numbers.py
@@ -1,28 +1,48 @@
-class Solution(object):
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
     def addTwoNumbers(self, l1, l2):
         """
         :type l1: ListNode
         :type l2: ListNode
         :rtype: ListNode
         """
+        carry = 0
+        result = ListNode(0)
+        curr = result
 
-        sum1, sum2 = 0, 0
+        while l1 or l2 or carry:
+            if l1:
+                carry += l1.val
+                l1 = l1.next
+            if l2:
+                carry += l2.val
+                l2 = l2.next
 
-        for digit in l1:
-            sum1 = sum1 * 10 + digit
-        for digit in l2:
-            sum2 = sum2 * 10 + digit
+            curr.next = ListNode(carry % 10)
+            carry //= 10
+            curr = curr.next
 
-        sum = sum1+sum2
+        return result.next
 
-        sum_list = [int(number) for number in str(sum)]
-        reverted_list = []
-        for i in range(len(sum_list) -1, -1, -1):
-            reverted_list.append(sum_list[i])
-        return reverted_list
 
-l1 = [2,4,3]
-l2 = [5,6,4]
+l1 = ListNode(2)
+l1.next = ListNode(4)
+l1.next.next = ListNode(3)
+
+l2 = ListNode(5)
+l2.next = ListNode(6)
+l2.next.next = ListNode(4)
+
 
 solution = Solution()
-print(solution.addTwoNumbers(l1, l2))
+
+result = solution.addTwoNumbers(l1, l2)
+
+
+while result:
+    print(result.val)
+    result = result.next
